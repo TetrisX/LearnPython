3f21edd32a82e7b1bbdeee2bc8071b43a266dbc5
Alexandr Rotar <tetrisx44@gmail.com>
2017-06-01 22:08:42 +0300

    Выполнил первые 2 задания

---
+++ les1_ex2.py
@@ -0,0 +1,12 @@
+def string_compare(string1, string2):
+
+    string1 = 'value'
+    string2 = 'dollar'
+    if string1 == string2:
+        return 1
+    elif string1 != string2 and len(string1) > len(string2):
+        return 2
+    elif string1 != string2 and string2 == 'learn':
+        return 3
+
+string_compare(string1, string2)
