4f5d274932d0ecf39332b4475b2582bb0f204067
Alexandr Rotar <tetrisx44@gmail.com>
2017-06-04 18:48:21 +0300

    добавил исключения

--- les1_ex4.py
+++ les1_ex4.py
@@ -1,15 +1,67 @@
-name_list = ["Вася", "Маша", "Петя", "Валера", "Саша", "Даша"]
-
-for name in name_list:
-    while name != "Валера":
-        name_list.pop(1)
-        print(name_list)
-        if name == "Валера":
-            print("Валера нашелся")
+# name_list = ["Вася", "Маша", "Петя", "Валера", "Саша", "Даша"]
 
 
+# name = None
+# while name != "Валера":
+#     name = name_list.pop(0)
+
+#     if name == "Валера":
+#         print("Валера нашелся!")
 
 
 
-def find_person(name1):
-    name1 = 
+# def find_person(name_input):
+#     name = None
+#     while name != name_input:
+#         name = name_list.pop(0)
+#         if name == name_input:
+#             print('Этот человек нашелся')
+#             break
+#         if len(name_list) == 0:
+#             print('Нет такого')
+#             break
+
+
+
+# find_person(input('Enter name : '))
+
+# def ask_user():
+#     while True:
+#         user_in = input('Как дела? :')
+#         if user_in == 'Хорошо':
+#             print('Молодец!')
+#             break
+
+
+# ask_user()
+
+# def get_answers(question):
+#     answers = {'привет': 'И тебе привет!', 
+#     'как дела': 'Лучше всех', 
+#     'пока': 'увидимся'}
+#     return answers.get(question.lower(), ' не знаю')
+
+
+
+def get_answers(question):
+    answers = {'какая погода': 'хорошая', 
+               'как дела': 'Лучше всех', 
+               'любимый цвет': 'синий'}
+    try:
+        return answers.get(question.lower(), ' не знаю')
+    except KeyboardInterrupt:
+        return 'НЕ НАЖИМАЙ ctrl+C!'
+
+def ask_user():
+
+    while True:
+        question = input('задай вопрос :')
+        if question == 'пока':
+            print('До встречи!')
+            break
+        print(get_answers(question))
+
+ask_user()
+
+
+
